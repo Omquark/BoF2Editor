6c78f634038895864288f098a801116860e40048
Clifford Huey <cliff.huey@gmail.com>
2023-05-05 19:31:46 -0400

    Initial commit

---
+++ back-end/src/main/java/com/omquark/BoF2Editor/BoF2EditorApplication.java
@@ -0,0 +1,13 @@
+package com.omquark.BoF2Editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoF2EditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoF2EditorApplication.class, args);
+	}
+
+}
---
+++ back-end/src/test/java/com/omquark/BoF2Editor/BoF2EditorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omquark.BoF2Editor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoF2EditorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
